6a1af321c7000892f235a0edee45ad42d8cad98d
a6haji <aly.haji99@gmail.com>
2017-01-21 18:02:59 -0500

    Created Proj

---
+++ OpenMind/OpenMind/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OpenMind
+//
+//  Created by Aly Haji on 2017-01-21.
+//  Copyright © 2017 Aly Haji. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OpenMind/OpenMind/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OpenMind
+//
+//  Created by Aly Haji on 2017-01-21.
+//  Copyright © 2017 Aly Haji. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
