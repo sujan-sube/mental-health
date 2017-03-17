a4433b373a21d564bf24006b7b4cfee49ba5e6e7
shirsks14 <shirsks14@gmail.com>
2017-03-17 11:58:04 -0400

    Implemented the tableview for dashboard

---
+++ OpenMind/OpenMind/TodayTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TodayTableViewController.swift
+//  OpenMind
+//
+//  Created by Shirshak Shrestha on 2017-03-17.
+//  Copyright © 2017 Aly Haji. All rights reserved.
+//
+
+import Foundation
