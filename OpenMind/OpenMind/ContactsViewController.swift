9856412d3621cd91acbe68385260b68649535b06
a6haji <aly.haji99@gmail.com>
2017-03-18 19:03:32 -0400

    Contacts Page

--- OpenMind/OpenMind/ContactsViewController.swift
+++ OpenMind/OpenMind/ContactsViewController.swift
@@ -9,27 +9,77 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
-
+    
+    @IBOutlet weak var MomImage: UIImageView!
+    @IBOutlet weak var DrImage: UIImageView!
+    @IBOutlet weak var DadImage: UIImageView!
+    @IBOutlet weak var MessageButton: UIButton!
+    @IBOutlet weak var MomCell: UIView!
+    @IBOutlet weak var DadCell: UIView!
+    @IBOutlet weak var DrCell: UIView!
+    
+    let messageComposer = MessageComposer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        MomCell.layer.cornerRadius = 5
+        MomCell.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor
+        
+        DadCell.layer.cornerRadius = 5
+        DadCell.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor
+        
+        DrCell.layer.cornerRadius = 5
+        DrCell.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor
+        
+        MomImage.image = #imageLiteral(resourceName: "Mompic")
+        DrImage.image = #imageLiteral(resourceName: "DrPic")
+        DadImage.image = #imageLiteral(resourceName: "Dadpic")
+        MessageButton.tintColor = UIColor.white
+        
+        let gradient: CAGradientLayer = CAGradientLayer()
+        
+        gradient.colors = [UIColor(red: 177.0/255.0, green: 222.0/255.0, blue: 253.0/255.0, alpha: 1.0).cgColor, UIColor(red: 75.0/255.0, green: 176.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.frame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        
+        self.view.layer.insertSublayer(gradient, at: 0)
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func MessageButton(_ sender: Any) {
+        if (messageComposer.canSendText()) {
+            // Obtain a configured MFMessageComposeViewController
+            let messageComposeVC = messageComposer.configuredMessageComposeViewController()
+            
+            // Present the configured MFMessageComposeViewController instance
+            // Note that the dismissal of the VC will be handled by the messageComposer instance,
+            // since it implements the appropriate delegate call-back
+            present(messageComposeVC, animated: true, completion: nil)
+        } else {
+            // Let the user know if his/her device isn't able to send text messages
+            let errorAlert = UIAlertView(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", delegate: self, cancelButtonTitle: "OK")
+            errorAlert.show()
+        }
+        
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ OpenMind/OpenMind/MessageComposer.swift
@@ -0,0 +1,33 @@
+//
+//  MessageComposer.swift
+//  OpenMind
+//
+//  Created by Aly Haji on 2017-03-18.
+//  Copyright © 2017 Aly Haji. All rights reserved.
+//
+
+import Foundation
+import MessageUI
+
+let textMessageRecipients = ["647-573-9425", "306-537-4629"] // for pre-populating the recipients list (optional, depending on your needs)
+
+class MessageComposer: NSObject, MFMessageComposeViewControllerDelegate {
+    
+    // A wrapper function to indicate whether or not a text message can be sent from the user's device
+    func canSendText() -> Bool {
+        return MFMessageComposeViewController.canSendText()
+    }
+    
+    // Configures and returns a MFMessageComposeViewController instance
+    func configuredMessageComposeViewController() -> MFMessageComposeViewController {
+        let messageComposeVC = MFMessageComposeViewController()
+        messageComposeVC.messageComposeDelegate = self  //  Make sure to set this property to self, so that the controller can be dismissed!
+        messageComposeVC.recipients = textMessageRecipients
+        messageComposeVC.body = "Hey guys, I'm not feeling too well"
+        return messageComposeVC
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
