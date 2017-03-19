95c73d196ffe88f93ecd0f5841dec6830bbf6e42
shirsks14 <shirsks14@gmail.com>
2017-03-18 21:42:09 -0400

    Changed dismiss color

--- OpenMind/OpenMind/InsightsTableViewCell.swift
+++ OpenMind/OpenMind/InsightsTableViewCell.swift
@@ -16,6 +16,7 @@ class InsightsTableViewCell: UITableViewCell {
     @IBAction func dismissoOnClick(_ sender: UIButton) {
     }
     @IBOutlet weak var insightsMainView: UIView!
+    @IBOutlet weak var insightsDismissView: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +24,7 @@ class InsightsTableViewCell: UITableViewCell {
         insightsMainView.layer.cornerRadius = 5
         insightsMainView.layer.masksToBounds = true
         insightsMainView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor
+        insightsDismissView.layer.backgroundColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 100/255.0, alpha: 1).cgColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
