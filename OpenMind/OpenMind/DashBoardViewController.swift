74f481349bcff11861c7e73789e9d005135674bd
shirsks14 <shirsks14@gmail.com>
2017-03-20 23:02:40 -0400

    Scrollview for trends with multiple graphs

--- OpenMind/OpenMind/DashBoardViewController.swift
+++ OpenMind/OpenMind/DashBoardViewController.swift
@@ -25,17 +25,25 @@ class DashBoardViewController: UIViewController {
     @IBOutlet weak var Sleep: UIButton!
     @IBOutlet weak var progressView: UIView!
     
+    @IBOutlet weak var progressShadowView: UIView!
+    @IBOutlet weak var emotionShadowView: UIView!
+    @IBOutlet weak var journalShadowView: UIView!
+    @IBOutlet weak var activityShadowView: UIView!
+    @IBOutlet weak var sleepShadowView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setupJournalAnalysisProgressBar()
         self.animateProgress(angle: 300)
         self.setupButtons()
-        //self.progressView.layer.backgroundColor = UIColor(red: 131/255, green: 197/255, blue: 249/255, alpha: 1).cgColor
-        self.progressView.layer.backgroundColor = UIColor(red: 131/255, green: 197/255, blue: 253/255, alpha: 1).cgColor
+        self.progressView.layer.backgroundColor = UIColor(red: 131/255, green: 197/255, blue: 249/255, alpha: 1).cgColor
+//        self.progressView.layer.backgroundColor = UIColor(red: 131/255, green: 197/255, blue: 253/255, alpha: 1).cgColor
         self.progressView.layer.borderWidth = CGFloat(buttonborderwidth)
         self.progressView.layer.borderColor = buttonbordercolor.cgColor
         // Do any additional setup after loading the view.
+        
+        self.createShadows()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -61,10 +69,10 @@ class DashBoardViewController: UIViewController {
     
     
     func setupButtons() -> Void {
-        self.Journal.backgroundColor = buttonBackgroundColor
-        self.Emotion.backgroundColor = buttonBackgroundColor
-        self.Activity.backgroundColor = buttonBackgroundColor
-        self.Sleep.backgroundColor = buttonBackgroundColor
+//        self.Journal.backgroundColor = buttonBackgroundColor
+//        self.Emotion.backgroundColor = buttonBackgroundColor
+//        self.Activity.backgroundColor = buttonBackgroundColor
+//        self.Sleep.backgroundColor = buttonBackgroundColor
         
         self.Journal.layer.borderWidth = CGFloat(buttonborderwidth)
         self.Journal.layer.borderColor = buttonbordercolor.cgColor
@@ -126,9 +134,60 @@ class DashBoardViewController: UIViewController {
         }
         return
     }
-
     
-    
+    func createShadows(){
+        
+        progressShadowView.layer.shadowPath = UIBezierPath(roundedRect: progressShadowView.layer.bounds, cornerRadius: 5).cgPath
+        //        insightsMainView.layer.shouldRasterize = true
+        progressShadowView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8).cgColor
+        progressShadowView.layer.shouldRasterize = true
+        progressShadowView.layer.cornerRadius = 5
+        progressShadowView.layer.shadowColor = UIColor.black.cgColor
+        progressShadowView.layer.shadowOpacity = 0.4
+        progressShadowView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        progressShadowView.layer.shadowRadius = 2.0
+        
+        emotionShadowView.layer.shadowPath = UIBezierPath(roundedRect: emotionShadowView.layer.bounds, cornerRadius: 5).cgPath
+        //        insightsMainView.layer.shouldRasterize = true
+        emotionShadowView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8).cgColor
+        emotionShadowView.layer.shouldRasterize = true
+        emotionShadowView.layer.cornerRadius = 5
+        emotionShadowView.layer.shadowColor = UIColor.black.cgColor
+        emotionShadowView.layer.shadowOpacity = 0.4
+        emotionShadowView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        emotionShadowView.layer.shadowRadius = 2.0
+        
+        journalShadowView.layer.shadowPath = UIBezierPath(roundedRect: journalShadowView.layer.bounds, cornerRadius: 5).cgPath
+        //        insightsMainView.layer.shouldRasterize = true
+        journalShadowView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8).cgColor
+        journalShadowView.layer.shouldRasterize = true
+        journalShadowView.layer.cornerRadius = 5
+        journalShadowView.layer.shadowColor = UIColor.black.cgColor
+        journalShadowView.layer.shadowOpacity = 0.4
+        journalShadowView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        journalShadowView.layer.shadowRadius = 2.0
+        
+        activityShadowView.layer.shadowPath = UIBezierPath(roundedRect: activityShadowView.layer.bounds, cornerRadius: 5).cgPath
+        //        insightsMainView.layer.shouldRasterize = true
+        activityShadowView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8).cgColor
+        activityShadowView.layer.shouldRasterize = true
+        activityShadowView.layer.cornerRadius = 5
+        activityShadowView.layer.shadowColor = UIColor.black.cgColor
+        activityShadowView.layer.shadowOpacity = 0.4
+        activityShadowView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        activityShadowView.layer.shadowRadius = 2.0
+        
+        sleepShadowView.layer.shadowPath = UIBezierPath(roundedRect: sleepShadowView.layer.bounds, cornerRadius: 5).cgPath
+        //        insightsMainView.layer.shouldRasterize = true
+        sleepShadowView.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8).cgColor
+        sleepShadowView.layer.shouldRasterize = true
+        sleepShadowView.layer.cornerRadius = 5
+        sleepShadowView.layer.shadowColor = UIColor.black.cgColor
+        sleepShadowView.layer.shadowOpacity = 0.4
+        sleepShadowView.layer.shadowOffset = CGSize.init(width: 1, height: 1)
+        sleepShadowView.layer.shadowRadius = 2.0
+        
+    }
 }
 
     /*
